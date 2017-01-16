39bc595c84adfdf0567ac16ce848a03130aac3df
kolyn236 <kolyn236@mail.ru>
2017-01-16 23:48:11 +0400

    Testing JS

--- php_for_me/views/template/index.php
+++ php_for_me/views/template/index.php
@@ -2,26 +2,33 @@
 <html>
 
 <head>
-
+    <title>Замечательная страница</title>
     <meta charset="utf-8">      
    <!--<script src="<?= JS_PATH ?>my.js"></script>-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script>
+    var need = document.getElementsByClassName("heed");
 
     </script>
 </head>
 
 <body>
-
+<h1 class="heed">Логотип</h1>
 <div>Пользователи:</div>
-<div id="alert">
-    <span class="span" style="background: blue">Кнопка</span>
-    <input value="Нажми на меня!" id="elem" type="button">
-</div>
-<ul>
-    <li>Маша</li>
-    <li>Вовочка</li>
-</ul>
+
+
+
+
+<h1>Заголовок № /h1>
+
+
+
+<input type="text" value="Текст" class="reddys">
+    <input type="button" value="Кнопка"/>
+
+
+
+
 <script>
     elem.onclick = function () {
         alert("Нажми на меня ещё раз сука!")
